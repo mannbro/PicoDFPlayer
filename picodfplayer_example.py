def54d8a9f0be3407090276aa1e78a4797a4b624
mannbro <mannbro@gmail.com>
2022-08-28 22:16:57 +0200

    Add files via upload

--- picodfplayer_example.py
+++ picodfplayer_example.py
@@ -10,14 +10,14 @@ from picodfplayer import DFPlayer
 UART_INSTANCE=0
 TX_PIN = 16
 RX_PIN=17
-BUSY_PIN=1
+BUSY_PIN=6
 
 #Create player instance
 player=DFPlayer(UART_INSTANCE, TX_PIN, RX_PIN, BUSY_PIN)
 
 
 #Check if player is busy.
-print('Playing', player.queryBusy())
+print('Playing?', player.queryBusy())
 #Play the first song (001.mp3) from the first folder (01)
 
 print('Playing track 001.mp3 in folder 01')
